2579c02f95463e4241da42460c01f6112803ed4d
Suman Tandan <87357770+tandon1999@users.noreply.github.com>
2025-09-20 21:41:47 +0545

    Parent branch/nc 5 (#6)
    * Backend Template Created (#1)
    
    Okay For Me
    
    * Database Created and Updated a appsetting.json (#3)
    
    Okay
    
    * Question Setup Created with the database (#5)

---
+++ API/RequestModel/Setup/QuestionSetupRequestModel.cs
@@ -0,0 +1,16 @@
+﻿namespace API.RequestModel.Setup
+{
+    public class QuestionSetupRequestModel
+    {
+        public int QID { get; set; }
+        public string? Question { get; set; }
+        public List<McqOption> Optionslist { get; set; } = new List<McqOption>();
+    }
+    public class McqOption
+    {
+        public int OptionId { get; set; }
+        public string Answer { get; set; } = string.Empty;
+        public bool IsCorrect { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ API/ResponseModel/Setup/QuestionSetupResponseModel.cs
@@ -0,0 +1,6 @@
+﻿namespace API.ResponseModel.Setup
+{
+    public class QuestionSetupResponseModel
+    {
+    }
+}
