d314c56efe450d3e3eaa71e7f459ba557595960b
Suman Tandan <87357770+tandon1999@users.noreply.github.com>
2025-09-20 21:40:55 +0545

    Question Setup Created with the database (#5)

---
+++ API/Controllers/Setup/QuestionSetupController.cs
@@ -0,0 +1,50 @@
+﻿using API.RequestModel.Setup;
+using API.Services.Interface.Setup;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Setup
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionSetupController : ControllerBase
+    {
+        private readonly IQuestionSetupService _questionSetupService;
+        public QuestionSetupController(IQuestionSetupService questionSetupService)
+        {
+            _questionSetupService = questionSetupService ?? throw new ArgumentNullException(nameof(questionSetupService));
+        }
+
+
+        [HttpPost("CreateQuestion")]
+        public async Task<IActionResult> CreateQuestion(QuestionSetupRequestModel question)
+        {
+            var response = await _questionSetupService.CreateQuestionAsync(question);
+            return Ok(response);
+        }
+
+
+        [HttpGet("GetQuestionById")]
+        public async Task<IActionResult> GetQuestionById(int id)
+        {
+            var response = await _questionSetupService.GetQuestionByIdAsync(id);
+            return Ok(response);
+        }
+
+
+        [HttpGet("GetAllQuestions")]
+        public async Task<IActionResult> GetAllQuestions()
+        {
+            var response = await _questionSetupService.GetAllQuestionsAsync();
+            return Ok(response);
+        }
+
+
+        [HttpDelete("DeleteQuestion")]
+        public async Task<IActionResult> DeleteQuestion(int id)
+        {
+            var response = await _questionSetupService.DeleteQuestionAsync(id);
+            return Ok(response);
+        }
+    }
+}
---
+++ API/Services/Interface/Setup/IQuestionSetupService.cs
@@ -0,0 +1,13 @@
+﻿using API.RequestModel.Setup;
+using Shared.Wrapper;
+
+namespace API.Services.Interface.Setup
+{
+    public interface IQuestionSetupService :IService
+    {
+        Task<IResponse> CreateQuestionAsync(QuestionSetupRequestModel question);
+        Task<IResponse<QuestionSetupRequestModel>> GetQuestionByIdAsync(int id);
+        Task<IResponse<List<QuestionSetupRequestModel>>> GetAllQuestionsAsync();
+        Task<IResponse> DeleteQuestionAsync(int id);
+    }
+}
